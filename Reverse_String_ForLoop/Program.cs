2163f36ae1f70f8af04252d6eac757154b22133b
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-09-16 14:35:31 -0400

    first commit

---
+++ Reverse_String_ForLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ForLoop
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            string name = "sea";
+            string output = "";
+            char [] input = name.ToCharArray();
+            for (int i = input.Length - 1; i >=0; i--)
+            {
+                output += input[i];
+            } 
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Reverse_String_WhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WhileLoop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Sea";
+            string output = "";
+            int length = name.Length - 1;
+            while(length >= 0)
+            {
+                output = output + name[length];
+                length --;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
